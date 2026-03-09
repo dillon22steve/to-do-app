12da6224cd2228745950e670a26d0d0113b192b7
dillon22steve <std42400@uga.edu>
2026-03-08 23:28:55 -0400

    Added login and signup buttons and forms to the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-quickstart';
+
+  showModal = false
+  isLogin = true
+
+  username = ''
+  password = ''
+
+  openLogin() {
+    this.isLogin = true
+    this.showModal = true
+  }
+
+  openSignup() {
+    this.isLogin = false
+    this.showModal = true
+  }
+
+  closeModal() {
+    this.showModal = false
+  }
+
+  submitForm() {
+    console.log(this.username, this.password)
+
+    if (this.isLogin) {
+      console.log("Logging in...")
+    } else {
+      console.log("Signing up...")
+    }
+
+    this.closeModal()
+  }
+
 }
